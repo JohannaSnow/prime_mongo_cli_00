0554d6dfaf141b8a2d04201860fa6a7d5b735edd
Johanna Snow <johannadsnow@gmail.com>
2016-09-21 16:51:13 -0500

    fixed something

--- queries.js
+++ queries.js
@@ -1,27 +1,30 @@
 Create a file, queries.js that will contain the queries that correctly address the criteria in the steps that follow.
 
-1. Find documents that have awards.
 
-  db.bios.find({awards: {$exists: true}}).pretty();
+ 1) Find documents that have awards:
 
-2. Find documents that don't have awards.
+    db.bios.find({awards: {$exists: true}}).pretty();
 
-  db.bios.find({awards: {$exists: false}}).pretty();
+2) Find documents that do not have awards:
 
-3.  Find documents that have contribs for OOP or UNIX.
-  db.bios.find({$or:[{"contribs": "OOP"}, {"contribs": "UNIX"}]}).pretty();
+    db.bios.find({awards: {$exists: false}}).pretty();
 
-4. Find documents with "Turing Award" awards.
-	db.bios.find( { "awards.award" : "Turing Award" }).pretty();
+3)  Find documents that have contribs for OOP or UNIX:
 
-5. Find documents with IDs between 3 and 7.
+    db.bios.find({$or:[{"contribs": "OOP"}, {"contribs": "UNIX"}]}).pretty();
 
-  db.bios.find({$and:[{"_id": {$gt:3}}, {"_id": {$lt:7}}]}).pretty();
+ 4)Find documents with "Turing Award" awards:
 
-6. Find documents with awards that were awarded before the year 2000.
+	  db.bios.find( { "awards.award" : "Turing Award" }).pretty();
 
-  db.bios.find({"awards.year": {$lt:2000}}).pretty();
+ 5)Find documents with IDs between 3 and 7:
 
-7. Find documents with birth dates, but no death dates.
+    db.bios.find({$and:[{"_id": {$gt:3}}, {"_id": {$lt:7}}]}).pretty();
 
-  db.bios.find({$and:[{birth: {$exists: true}}, {death: {$exists: false}}]}).pretty();
+6) Find documents with awards that were awarded before the year 2000:
+
+    db.bios.find({"awards.year": {$lt:2000}}).pretty();
+
+ 7)  Find documents with birth dates, but no death dates:
+
+    db.bios.find({$and:[{birth: {$exists: true}}, {death: {$exists: false}}]}).pretty();
